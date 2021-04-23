cde39dc3a81914a8fe91c6109e68bc7ad8962739
Allen Guo <elon.guo@gmail.com>
2021-04-23 15:23:55 +0800

    add koda-react-widgets dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// @ts-ignore
 import { ColorPickerField, ImageField } from 'koda-react-widgets';
 import logo from './logo.svg';
 import './App.css';
@@ -7,21 +8,8 @@ console.log(ColorPickerField, ImageField);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.
-        </a>
+        <ImageField />
         <ColorPickerField />
-      </header>
     </div>
   );
 }
